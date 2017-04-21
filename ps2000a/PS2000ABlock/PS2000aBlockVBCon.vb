d463a49004c8f1af56366349891f8387ff00891e
hitesh.mistry <hsmistry@users.noreply.github.com>
2017-04-21 14:29:03 +0100

    Added ps2000a example

---
+++ ps2000a/PS2000ABlock/PS2000aBlockVBCon.vb
@@ -0,0 +1,435 @@
+﻿'===================================================================================================
+'
+'	Filename:			PS2000ABlockVBCon.vb
+'
+'	Description: 
+'	    This file demonstrates how to use VB .NET on the PicoScope 2000 series oscilloscopes using 
+'       the ps2000a driver.
+'       The application shows how to connect to a device, display device information and collect 
+'       data in block mode.
+'
+'   Copyright (C) 2017 Pico Technology Ltd. See LICENSE file for terms.
+'
+'===================================================================================================
+
+Imports System.IO
+
+Module PS2000ABlockVBCon
+
+    ' Delegate
+    Public ps2000aBlockCallback As ps2000aBlockReady
+
+    ' Define variables
+
+    Dim handle As Short
+    Dim status As UInteger
+    Dim serial As String
+    Dim maxADCValue As Short
+    Dim channelCount As Short
+    Dim totalSamples As UInteger
+
+    Public deviceReady As Boolean = False
+    Public isMSODevice As Boolean = False
+
+    ' ******************************************************************************************************************************************************************
+    ' GetDeviceInfo - Reads and displays the scopes device information.
+    '
+    ' Parameters: handle - the device handle
+    ' *******************************************************************************************************************************************************************
+    Sub getDeviceInfo(ByVal handle As Short)
+
+        Dim infoText(8) As String
+        Dim infoStr As String
+        Dim infoStr2 As String
+        Dim requiredSize As Integer
+        Dim i As Integer
+        Dim chrs(5) As Char
+
+        infoText(0) = "Driver Ver:        "
+        infoText(1) = "USB Ver:           "
+        infoText(2) = "Hardware Ver:      "
+        infoText(3) = "Variant:           "
+        infoText(4) = "Batch / Serial:    "
+        infoText(5) = "Cal Date:          "
+        infoText(6) = "Kernel Driver Ver: "
+        infoText(7) = "Digital H/W Ver:   "
+        infoText(8) = "Analogue H/W Ver:  "
+
+        For i = 0 To infoText.Length - 1
+
+            infoStr = "                    "
+            ps2000aGetUnitInfo(handle, infoStr, CShort(infoStr.Length), requiredSize, i)
+            infoStr2 = infoStr.TrimEnd(" ")
+            infoText(i) += infoStr
+            Console.WriteLine(infoText(i) & vbTab)
+
+            If i = PicoInfo.PICO_VARIANT_INFO Then
+
+                channelCount = Convert.ToInt16(infoStr2.ToCharArray(1, 1))
+
+                If infoStr2.EndsWith("MSO", StringComparison.CurrentCultureIgnoreCase) Then
+
+                    isMSODevice = True
+
+                Else
+
+                    isMSODevice = False
+
+                End If
+
+            End If
+
+        Next i
+
+        Console.WriteLine(vbNewLine)
+
+    End Sub
+
+
+
+    Sub Main()
+
+        Console.WriteLine("PicoScope 2000 Series (ps2000a) VB .NET Block Capture Example Program" _
+                          & vbNewLine & "=====================================================================" & vbNewLine)
+
+        ' Device Connection and Setup
+        ' ===========================
+
+        ' Open device 
+        status = ps2000aOpenUnit(handle, vbNullString)
+
+        If status = PicoStatus.PICO_OK Then
+
+            Console.WriteLine("Device opened successfully:-" & vbNewLine)
+
+        ElseIf status = PicoStatus.PICO_NOT_FOUND Then
+
+            Console.WriteLine("Device not found." & vbNewLine)
+            Sleep(2000)
+            Exit Sub
+
+        Else
+
+            Console.WriteLine("Device not opened." & vbNewLine)
+            Sleep(2000)
+            Exit Sub
+
+        End If
+
+        ' Find maximum ADC count this value is used for scaling values between ADC counts and millivolts
+        status = ps2000aMaximumValue(handle, maxADCValue)
+
+        'Read and display the device information
+        getDeviceInfo(handle)
+
+        ' Channel Setup
+        ' -------------
+
+        ' Setup Channel A - 5V range, DC coupling, 0 Analogue offset
+
+        status = ps2000aSetChannel(handle, PS2000AImports.Channel.PS2000A_CHANNEL_A, CShort(1), PS2000AImports.CouplingMode.PS2000A_DC, PS2000AImports.VoltageRange.PS2000A_5V, 0.0)
+
+        If status <> PICO_OK Then
+
+            Console.WriteLine("Error setting channel A")
+            Call ErrorHandler(handle, status, "ps2000aSetChannel")
+            Exit Sub
+
+        End If
+
+        ' Turn off other channels
+
+        ' Turn off Channel B
+        status = ps2000aSetChannel(handle, PS2000AImports.Channel.PS2000A_CHANNEL_B, CShort(0), PS2000AImports.CouplingMode.PS2000A_DC, PS2000AImports.VoltageRange.PS2000A_5V, 0.0)
+
+        If status <> PICO_OK Then
+
+            Console.WriteLine("Error setting channel B")
+            Call ErrorHandler(handle, status, "ps2000aSetChannel")
+            Exit Sub
+
+        End If
+
+        ' Only disable Channels C and D if it is a 4-channel device 
+
+        If channelCount = 4 Then
+
+            status = ps2000aSetChannel(handle, PS2000AImports.Channel.PS2000A_CHANNEL_C, CShort(0), PS2000AImports.CouplingMode.PS2000A_DC, PS2000AImports.VoltageRange.PS2000A_5V, 0.0)
+
+            If status <> PICO_OK Then
+
+                Console.WriteLine("Error setting channel C")
+                Call ErrorHandler(handle, status, "ps2000aSetChannel")
+                Exit Sub
+
+            End If
+
+            status = ps2000aSetChannel(handle, PS2000AImports.Channel.PS2000A_CHANNEL_D, CShort(0), PS2000AImports.CouplingMode.PS2000A_DC, PS2000AImports.VoltageRange.PS2000A_5V, 0.0)
+
+            If status <> PICO_OK Then
+
+                Console.WriteLine("Error setting channel D")
+                Call ErrorHandler(handle, status, "ps2000aSetChannel")
+                Exit Sub
+
+            End If
+
+        End If
+
+        ' If the device is a Mixed Signal Oscilloscope, turn off any Digital Ports
+
+        If isMSODevice = True Then
+
+            status = ps2000aSetDigitalPort(handle, DigitalPort.PS2000A_DIGITAL_PORT0, CShort(0), CShort(0))
+
+            If status <> PICO_OK Then
+
+                Console.WriteLine("Error setting Digital PORT0")
+                Call ErrorHandler(handle, status, "ps2000aSetChannel")
+                Exit Sub
+
+            End If
+
+            status = ps2000aSetDigitalPort(handle, DigitalPort.PS2000A_DIGITAL_PORT1, CShort(0), CShort(0))
+
+            If status <> PICO_OK Then
+
+                Console.WriteLine("Error setting Digital PORT1")
+                Call ErrorHandler(handle, status, "ps2000aSetChannel")
+                Exit Sub
+
+            End If
+
+        End If
+
+        ' Verify Timebase for sampling interval and maximum number of samples
+        ' -------------------------------------------------------------------
+
+        ' Confirm timebase corresponding to desired sampling interval is valid.
+        ' Timebase indices can be calculated using the formulae in the Timebases section of the Programmer's Guide
+
+        Dim timebase As UInteger
+        Dim numPreTriggerSamples As Integer
+        Dim numPostTriggerSamples As Integer
+        Dim timeIntervalNs As Integer
+        Dim maxSamples As Integer
+        Dim segmentIndex As UInteger
+        Dim getTimebase2Status As UInteger
+
+        timebase = 65
+        numPreTriggerSamples = 0
+        numPostTriggerSamples = 10000
+        totalSamples = numPreTriggerSamples + numPostTriggerSamples
+
+        timeIntervalNs = CSng(0.0)
+        maxSamples = 0
+        segmentIndex = 0
+        getTimebase2Status = PicoStatus.PICO_INVALID_TIMEBASE ' Initialise as invalid timebase
+
+
+        Do Until getTimebase2Status = PicoStatus.PICO_OK
+
+            getTimebase2Status = ps2000aGetTimebase2(handle, timebase, totalSamples, timeIntervalNs, CShort(1), maxSamples, segmentIndex)
+
+            If getTimebase2Status <> PicoStatus.PICO_OK Then
+
+                timebase = timebase + 1
+
+            End If
+
+        Loop
+
+        Console.WriteLine("Timebase: {0} Sample interval: {1} ns, Max Samples: {2}", timebase, timeIntervalNs, maxSamples, vbNewLine)
+
+        ' Setup trigger
+        ' -------------
+
+        ' Setup a trigger such that the device will trigger on a rising edge through 500 mV on Channel A
+
+        Dim threshold As Short
+        Dim delay As UInteger
+        Dim autoTriggerMs As Short
+
+        ' Convert the threshold from millivolts to an ADC count
+        threshold = PicoFunctions.mvToAdc(500, PS2000AImports.VoltageRange.PS2000A_5V, maxADCValue)
+
+        Console.WriteLine("Trigger threshold: {0} mV ({1} ADC Counts)", 500, threshold, vbNewLine)
+
+        delay = 0
+        autoTriggerMs = 2000 ' Auto-trigger after 2 seconds if trigger event has not occurred
+
+        status = ps2000aSetSimpleTrigger(handle, CShort(1), PS2000AImports.Channel.PS2000A_CHANNEL_A, threshold, PS2000AImports.ThresholdDirection.PS2000A_RISING, delay, autoTriggerMs)
+
+        If status <> PICO_OK Then
+
+            Call ErrorHandler(handle, status, "ps2000aSetSimpleTrigger")
+            Exit Sub
+
+        End If
+
+        ' Signal Generator
+        ' ================
+
+        ' Use in-built function generator in order to provide a test signal (1 KHz, 2 Vpp sine wave)
+
+        Console.WriteLine("Connect the AWG output to Channel A and press <Enter> to continue.")
+
+        Do While Console.ReadKey().Key <> ConsoleKey.Enter
+        Loop
+
+        status = ps2000aSetSigGenBuiltInV2(handle, 0, CUInt(2000000), WaveType.PS2000A_SINE, 1000.0, 1000.0, 0.0, 0.0, SweepType.PS2000A_UP, ExtraOperations.PS2000A_ES_OFF, _
+                                           CUInt(0), CUInt(0), SigGenTrigType.PS2000A_SIGGEN_RISING, SigGenTrigSource.PS2000A_SIGGEN_NONE, CShort(0))
+
+        If status <> PICO_OK Then
+
+            Call ErrorHandler(handle, status, "ps2000aSetSigGenBuiltInV2")
+            Exit Sub
+
+        End If
+
+        ' Data Capture
+        ' ============
+
+        ' Capture block of data from the device using a trigger.
+
+        ' Create instance of delegate (callback function)
+        ps2000aBlockCallback = New ps2000aBlockReady(AddressOf BlockCallback)
+
+        Dim timeIndisposedMs As Integer = 0
+
+        Console.WriteLine("Collecting {0} samples...", (numPreTriggerSamples + numPostTriggerSamples), vbNewLine)
+        Console.WriteLine("Press any key to cancel data collection.")
+
+        status = ps2000aRunBlock(handle, numPreTriggerSamples, numPostTriggerSamples, timebase, CShort(1), timeIndisposedMs, segmentIndex, ps2000aBlockCallback, IntPtr.Zero)
+
+        If status <> PICO_OK Then
+            
+            Call ErrorHandler(handle, status, "ps2000aRunBlock")
+            Exit Sub
+
+        End If
+
+        Dim ready As Short = 0
+
+        ' Wait for the device to become ready (i.e. complete data collection)
+        While deviceReady = False AndAlso Console.KeyAvailable = False
+
+            Threading.Thread.Sleep(50)
+
+        End While
+
+        If Console.KeyAvailable Then
+
+                ' clear the key
+                Console.ReadKey(True)
+
+            End If
+
+            Console.WriteLine()
+
+        If deviceReady = True Then
+
+            ' Setup data buffers to retrieve data values - this can also be carried out prior to data collection
+
+            Dim valuesChA() As Short
+            ReDim valuesChA(totalSamples - 1)
+
+            status = ps2000aSetDataBuffer(handle, PS2000AImports.Channel.PS2000A_CHANNEL_A, valuesChA(0), CInt(totalSamples), segmentIndex, PS2000AImports.RatioMode.PS2000A_RATIO_MODE_NONE)
+
+            ' Retrieve values
+
+            Dim startIndex As UInteger
+            Dim downSampleRatio As UInteger
+            Dim overflow As Short
+
+            startIndex = 0
+            downSampleRatio = 1
+            overflow = 0
+
+            status = ps2000aGetValues(handle, startIndex, totalSamples, downSampleRatio, PS2000AImports.RatioMode.PS2000A_RATIO_MODE_NONE, segmentIndex, overflow)
+
+            If status <> PICO_OK Then
+
+                Call ErrorHandler(handle, status, "ps2000aGetValues")
+                Exit Sub
+
+            End If
+
+            Console.WriteLine("Retrieved {0} samples.", totalSamples, vbNewLine)
+
+
+            ' Process the Data
+            ' ----------------
+
+            ' In this example, the data is converted to millivolts and output to file
+
+            Dim valuesChAMv() As Single
+            ReDim valuesChAMv(totalSamples - 1)
+
+            Dim fileName As String
+            fileName = "ps2000a_triggered_block.txt"
+
+            Using outfile As New StreamWriter(fileName)
+
+                outfile.WriteLine("Triggered Block Data")
+                outfile.WriteLine("Data is output in millivolts.")
+                outfile.WriteLine()
+                outfile.WriteLine("Channel A")
+                outfile.WriteLine()
+
+                For index = 0 To totalSamples - 1
+
+                    valuesChAMv(index) = PicoFunctions.adcToMv(valuesChA(index), PS2000AImports.VoltageRange.PS2000A_5V, maxADCValue) ' Use the voltage range specified in the call to ps2000aSetChannel
+                    outfile.Write(valuesChAMv(index).ToString)
+                    outfile.WriteLine()
+
+                Next
+
+            End Using
+
+            Console.WriteLine("Data written to file {0}", fileName)
+
+        Else
+
+            Console.WriteLine("Data collection cancelled.")
+
+        End If
+
+        ' Reset the deviceReady flag in case we wish to collect another block of data
+        deviceReady = True
+
+        ' Stop the device
+        status = ps2000aStop(handle)
+
+        ' Close the connection to the device
+        ps2000aCloseUnit(handle)
+
+        Console.WriteLine(vbNewLine)
+
+        Console.WriteLine("Exiting application..." & vbNewLine)
+
+        Sleep(5000)
+
+    End Sub
+
+    ' Block callback function
+
+    Private Sub BlockCallback(ByVal handle As Short, ByVal status As UInteger, pVoid As IntPtr)
+
+        ' Flag to say done reading data
+        If status <> PicoStatus.PICO_CANCELLED Then
+            deviceReady = True
+        End If
+
+    End Sub
+
+    Private Sub ErrorHandler(ByVal handle As Short, ByVal status As UInteger, ByVal functionName As String)
+
+        Console.WriteLine("Error: {0} returned with status code {1} (0x{2})", functionName, status, Hex(status))
+        Call ps2000aCloseUnit(handle)
+        Console.WriteLine("Exiting application...")
+
+        Sleep(5000)
+    End Sub
+
+End Module
---
+++ ps2000a/PS2000AImports.vb
@@ -0,0 +1,263 @@
+﻿'===================================================================================================
+'	Filename:			PS2000AImports.vb
+'
+'	Description: 
+'	    This file defines enumerations and structures from the ps2000aApi.h C header file.
+'
+'===================================================================================================
+Module PS2000AImports
+
+    ' Constant values
+    ' ===============
+
+    ' Define any constant values here as required.
+
+
+    ' Enumerations
+    ' ============
+
+    Enum Channel
+        PS2000A_CHANNEL_A
+        PS2000A_CHANNEL_B
+        PS2000A_CHANNEL_C
+        PS2000A_CHANNEL_D
+        PS2000A_EXTERNAL ' PicoScope 2206/7/8 only
+    End Enum
+
+    Enum ChannelBufferIndex
+        PS2000A_CHANNEL_A_MAX
+        PS2000A_CHANNEL_A_MIN
+        PS2000A_CHANNEL_B_MAX
+        PS2000A_CHANNEL_B_MIN
+        PS2000A_CHANNEL_C_MAX
+        PS2000A_CHANNEL_C_MIN
+        PS2000A_CHANNEL_D_MAX
+        PS2000A_CHANNEL_D_MIN
+    End Enum
+
+    Enum CouplingMode
+        PS2000A_AC
+        PS2000A_DC
+    End Enum
+
+    Enum DigitalPort
+        PS2000A_DIGITAL_PORT0 = 128     ' digital channel 0 - 7
+        PS2000A_DIGITAL_PORT1           ' digital channel 8 - 15
+        PS2000A_DIGITAL_PORT2           ' digital channel 16 - 23
+        PS2000A_DIGITAL_PORT3           ' digital channel 24 - 31
+        PS2000A_MAX_DIGITAL_PORTS = (PS2000A_DIGITAL_PORT3 - PS2000A_DIGITAL_PORT0) + 1
+    End Enum
+
+    Enum EtsMode
+        PS2000A_ETS_OFF     'ETS disabled
+        PS2000A_ETS_FAST
+        PS2000A_ETS_SLOW
+        PS2000A_ETS_MODES_MAX
+    End Enum
+
+    Enum ExtraOperations
+        PS2000A_ES_OFF
+        PS2000A_WHITENOISE
+        PS2000A_PRBS 'Pseudo-Random Bit Stream 
+    End Enum
+
+    Enum IndexMode
+        PS2000A_SINGLE
+        PS2000A_DUAL
+        PS2000A_QUAD
+        PS2000A_MAX_INDEX_MODES
+    End Enum
+
+    Enum PulseWidthType
+        PS2000A_PW_TYPE_NONE
+        PS2000A_PW_TYPE_LESS_THAN
+        PS2000A_PW_TYPE_GREATER_THAN
+        PS2000A_PW_TYPE_IN_RANGE
+        PS2000A_PW_TYPE_OUT_OF_RANGE
+    End Enum
+
+    Enum RatioMode
+        PS2000A_RATIO_MODE_NONE = 0
+        PS2000A_RATIO_MODE_AGGREGATE = 1
+        PS2000A_RATIO_MODE_DECIMATE = 2
+        PS2000A_RATIO_MODE_AVERAGE = 4
+    End Enum
+
+    Enum SigGenTrigSource
+        PS2000A_SIGGEN_NONE
+        PS2000A_SIGGEN_SCOPE_TRIG
+        PS2000A_SIGGEN_AUX_IN
+        PS2000A_SIGGEN_EXT_IN
+        PS2000A_SIGGEN_SOFT_TRIG
+    End Enum
+
+    Enum SigGenTrigType
+        PS2000A_SIGGEN_RISING
+        PS2000A_SIGGEN_FALLING
+        PS2000A_SIGGEN_GATE_HIGH
+        PS2000A_SIGGEN_GATE_LOW
+    End Enum
+
+    Enum SweepType
+        PS2000A_UP
+        PS2000A_DOWN
+        PS2000A_UPDOWN
+        PS2000A_DOWNUP
+        PS2000A_MAX_SWEEP_TYPES
+    End Enum
+
+    Enum ThresholdDirection
+        'Above=0, Below=1, Rising / None=2, Falling=3, Rising_Or_Falling=4, Above_Lower=5, Below_Lower=6, Rising_Lower=7, Falling_Lower=8,
+        PS2000A_ABOVE                   ' using upper threshold
+        PS2000A_BELOW                   ' using upper threshold
+        PS2000A_RISING                  ' using upper threshold
+        PS2000A_NONE = PS2000A_RISING   ' no trigger set
+        PS2000A_FALLING                 ' using upper threshold
+        PS2000A_RISING_OR_FALLING       ' using both threshold
+        PS2000A_ABOVE_LOWER             ' using lower threshold
+        PS2000A_BELOW_LOWER             ' using lower threshold
+        PS2000A_RISING_LOWER            ' using lower threshold
+        PS2000A_FALLING_LOWER           ' using lower threshold
+        ' Windowing using both thresholds
+        PS2000A_INSIDE = PS2000A_ABOVE
+        PS2000A_OUTSIDE = PS2000A_BELOW
+        PS2000A_ENTER = PS2000A_RISING
+        PS2000A_EXIT = PS2000A_FALLING
+        PS2000A_ENTER_OR_EXIT = PS2000A_RISING_OR_FALLING
+        PS2000A_POSITIVE_RUNT = 9
+        PS2000A_NEGATIVE_RUNT
+    End Enum
+
+    Enum ThresholdMode
+        LEVEL
+        WINDOW
+    End Enum
+
+    Enum TimeUnits
+        PS2000A_FS
+        PS2000A_PS
+        PS2000A_NS
+        PS2000A_US
+        PS2000A_MS
+        PS2000A_S
+    End Enum
+
+    Enum TriggerState
+        CONDITION_DONT_CARE
+        CONDITION_TRUE
+        CONDITION_FALSE
+        CONDITION_MAX
+    End Enum
+
+    Enum VoltageRange
+        PS2000A_10MV
+        PS2000A_20MV
+        PS2000A_50MV
+        PS2000A_100MV
+        PS2000A_200MV
+        PS2000A_500MV
+        PS2000A_1V
+        PS2000A_2V
+        PS2000A_5V
+        PS2000A_10V
+        PS2000A_20V
+        PS2000A_50V
+    End Enum
+
+    Enum WaveType
+        PS2000A_SINE
+        PS2000A_SQUARE
+        PS2000A_TRIANGLE
+        PS2000A_RAMP_UP
+        PS2000A_RAMP_DOWN
+        PS2000A_SINC
+        PS2000A_GAUSSIAN
+        PS2000A_HALF_SINE
+        PS2000A_DC_VOLTAGE
+        PS2000A_WHITE_NOISE
+        PS2000A_MAX_WAVE_TYPES
+    End Enum
+
+
+    ' Function Declarations
+    ' =====================
+
+    ' Device Connection and Setup Functions
+    ' -------------------------------------
+
+    Declare Function ps2000aOpenUnit Lib "ps2000a.dll" (ByRef handle As Short, ByVal serial As String) As UInteger
+    Declare Sub ps2000aCloseUnit Lib "ps2000a.dll" (ByVal handle As Short)
+
+    Declare Function ps2000aGetUnitInfo Lib "ps2000a.dll" (ByVal handle As Short, ByVal str As String, ByVal stringLength As Short, ByRef requiredSize As Short, ByVal info As UInteger) As UInteger
+    Declare Function ps2000aSetChannel Lib "ps2000a.dll" (ByVal handle As Short, ByVal channel As Channel, ByVal enabled As Short, ByVal dc As CouplingMode, ByVal range As VoltageRange,
+                                                          ByVal analogueOffset As Single) As UInteger
+
+    Declare Function ps2000aSetDigitalPort Lib "ps2000a.dll" (ByVal handle As Short, ByVal port As DigitalPort, ByVal enabled As Short, ByVal logicLevel As Short) As UInteger
+    Declare Function ps2000aMaximumValue Lib "ps2000a.dll" (ByVal handle As Short, ByRef value As Short) As UInteger
+
+    ' Block functions
+    ' ---------------
+
+    Declare Function ps2000aGetTimebase2 Lib "ps2000a.dll" (ByVal handle As Short, ByVal timebase As UInteger, ByVal noSamples As Integer, ByRef timeIntervalNs As Single, ByVal oversample As Short,
+                                                            ByRef maxSamples As Integer, ByVal segmentIndex As UInteger) As UInteger
+
+    Declare Function ps2000aGetValues Lib "ps2000a.dll" (ByVal handle As Short, ByVal startIndex As UInteger, ByRef numSamples As UInteger, ByVal downSampleRatio As UInteger,
+                                                         ByVal downSampleRatioMode As RatioMode, ByVal segmentIndex As UInteger, ByRef overflow As Short) As UInteger
+
+    Declare Function ps2000aIsReady Lib "ps2000a.dll" (ByVal handle As Short, ByRef ready As Short) As UInteger
+
+    Declare Function ps2000aRunBlock Lib "ps2000a.dll" (ByVal handle As Short, ByVal noOfPreTriggerSamples As Integer, ByVal noOfPostTriggerSamples As Integer, ByVal timebase As UInteger,
+                                                        ByVal oversample As Short, ByRef timeIndisposedMs As Integer, ByVal segmentIndex As UInteger, ByVal lpps2000aBlockReady As ps2000aBlockReady,
+                                                        ByVal pParam As IntPtr) As UInteger
+
+    ' Trigger Functions
+    ' -----------------
+
+    Declare Function ps2000aSetSimpleTrigger Lib "ps2000a.dll" (ByVal handle As Short, ByVal enable As Short, ByVal channel As Channel, ByVal threshold As Short, ByVal direction As ThresholdDirection,
+                                                                ByVal delay As UInteger, ByVal autoTriggerMs As Short) As UInteger
+
+
+    ' Functions relevant to all Data Capture Modes
+    ' --------------------------------------------
+
+    Declare Function ps2000aSetDataBuffer Lib "ps2000a.dll" (ByVal handle As Short, ByVal channelOrPort As Integer, ByRef buffer As Short, ByVal length As Integer, ByVal segmentIndex As UInteger,
+                                                             ByVal downSampleRatioMode As RatioMode) As UInteger
+    Declare Function ps2000aStop Lib "ps2000a.dll" (ByVal handle As Short) As UInteger
+
+    ' Signal Generator Functions
+    ' --------------------------
+
+    Declare Function ps2000aSetSigGenArbitrary Lib "ps2000a.dll" (ByVal handle As Short, ByVal offsetVoltage As Integer, ByVal peakToPeak As UInteger, ByVal startDeltaPhase As UInteger,
+                                                                  ByVal stopDeltaPhase As UInteger, ByVal deltaPhaseIncrement As UInteger, ByVal dwellCount As UInteger, ByRef arbitraryWaveform As Short,
+                                                                  ByVal arbitraryWaveformSize As Integer, ByVal sweepType As SweepType, ByVal operation As ExtraOperations, ByVal indexMode As IndexMode,
+                                                                  ByVal shots As UInteger, ByVal sweeps As UInteger, ByVal triggerType As SigGenTrigType, ByVal triggerSource As SigGenTrigSource,
+                                                                  ByVal extInThreshold As Short) As UInteger
+
+    Declare Function ps2000aSetSigGenBuiltInV2 Lib "ps2000a.dll" (ByVal handle As Short, ByVal offsetVoltage As Integer, ByVal peakToPeak As UInteger, ByVal waveType As WaveType,
+                                                                  ByVal startFrequency As Double, ByVal stopFrequency As Double, ByVal increment As Double, ByVal dwellTime As Double,
+                                                                  ByVal sweepType As SweepType, ByVal operation As ExtraOperations, ByVal shots As UInteger, ByVal sweeps As UInteger,
+                                                                  ByVal triggerType As SigGenTrigType, ByVal triggerSource As SigGenTrigSource, ByVal extInThreshold As Short) As UInteger
+
+    Declare Function ps2000aSigGenArbitraryMinMaxValues Lib "ps2000a.dll" (ByVal handle As Short, ByRef minArbWaveformValue As Short, ByRef maxArbWaveformValue As Short,
+                                                                           ByRef minArbWaveformSize As UInteger, ByRef maxArbWaveformSize As UInteger) As UInteger
+
+    Declare Function ps2000aSigGenFrequencyToPhase Lib "ps2000a.dll" (ByVal handle As Short, ByVal frequency As Double, ByVal indexMode As IndexMode, ByVal bufferLength As UInteger,
+                                                                      ByRef phase As UInteger) As UInteger
+
+    Declare Function ps2000aSigGenSoftwareControl Lib "ps2000a.dll" (ByVal handle As Short, ByVal state As Short) As UInteger
+
+
+    ' Delegate declarations
+    ' =====================
+
+    ' Block mode
+    ' ----------
+    Public Delegate Sub ps2000aBlockReady(handle As Short, status As UInteger, pVoid As IntPtr)
+
+
+    ' Other Functions
+    ' ===============
+
+    Declare Sub Sleep Lib "kernel32.dll" (ByVal millseconds As Integer)
+
+End Module
