048923221007b3027f2da52c7ea902f64e58bf1c
hitesh.mistry <hsmistry@users.noreply.github.com>
2017-06-01 15:00:41 +0100

    Added ps2000 driver block GUI example

---
+++ ps2000/PS2000BlockGui/PS2000BlockVBGui.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PS2000BlockVBGui
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.openButton = New System.Windows.Forms.Button()
+        Me.closeButton = New System.Windows.Forms.Button()
+        Me.unitInfoTextBox = New System.Windows.Forms.TextBox()
+        Me.getBlockDataButton = New System.Windows.Forms.Button()
+        Me.unitInfoLabel = New System.Windows.Forms.Label()
+        Me.timebaseLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.timebaseNumericUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.numSamplesNumericUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.samplingIntervalLabel = New System.Windows.Forms.Label()
+        Me.samplingIntervalTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.numSamplesCollectedTextBox = New System.Windows.Forms.TextBox()
+        Me.dataTextBox = New System.Windows.Forms.TextBox()
+        Me.dataLabel = New System.Windows.Forms.Label()
+        CType(Me.timebaseNumericUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numSamplesNumericUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'openButton
+        '
+        Me.openButton.Location = New System.Drawing.Point(12, 12)
+        Me.openButton.Name = "openButton"
+        Me.openButton.Size = New System.Drawing.Size(75, 23)
+        Me.openButton.TabIndex = 0
+        Me.openButton.Text = "Open"
+        Me.openButton.UseVisualStyleBackColor = True
+        '
+        'closeButton
+        '
+        Me.closeButton.Enabled = False
+        Me.closeButton.Location = New System.Drawing.Point(107, 12)
+        Me.closeButton.Name = "closeButton"
+        Me.closeButton.Size = New System.Drawing.Size(75, 23)
+        Me.closeButton.TabIndex = 1
+        Me.closeButton.Text = "Close"
+        Me.closeButton.UseVisualStyleBackColor = True
+        '
+        'unitInfoTextBox
+        '
+        Me.unitInfoTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.unitInfoTextBox.Location = New System.Drawing.Point(12, 68)
+        Me.unitInfoTextBox.Multiline = True
+        Me.unitInfoTextBox.Name = "unitInfoTextBox"
+        Me.unitInfoTextBox.ReadOnly = True
+        Me.unitInfoTextBox.Size = New System.Drawing.Size(170, 139)
+        Me.unitInfoTextBox.TabIndex = 2
+        '
+        'getBlockDataButton
+        '
+        Me.getBlockDataButton.Enabled = False
+        Me.getBlockDataButton.Location = New System.Drawing.Point(233, 138)
+        Me.getBlockDataButton.Name = "getBlockDataButton"
+        Me.getBlockDataButton.Size = New System.Drawing.Size(104, 23)
+        Me.getBlockDataButton.TabIndex = 3
+        Me.getBlockDataButton.Text = "Get Block Data"
+        Me.getBlockDataButton.UseVisualStyleBackColor = True
+        '
+        'unitInfoLabel
+        '
+        Me.unitInfoLabel.AutoSize = True
+        Me.unitInfoLabel.Location = New System.Drawing.Point(12, 52)
+        Me.unitInfoLabel.Name = "unitInfoLabel"
+        Me.unitInfoLabel.Size = New System.Drawing.Size(50, 13)
+        Me.unitInfoLabel.TabIndex = 4
+        Me.unitInfoLabel.Text = "Unit Info:"
+        '
+        'timebaseLabel
+        '
+        Me.timebaseLabel.AutoSize = True
+        Me.timebaseLabel.Location = New System.Drawing.Point(202, 70)
+        Me.timebaseLabel.Name = "timebaseLabel"
+        Me.timebaseLabel.Size = New System.Drawing.Size(56, 13)
+        Me.timebaseLabel.TabIndex = 5
+        Me.timebaseLabel.Text = "Timebase:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(202, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Number of samples:"
+        '
+        'timebaseNumericUpDown
+        '
+        Me.timebaseNumericUpDown.Location = New System.Drawing.Point(329, 68)
+        Me.timebaseNumericUpDown.Maximum = New Decimal(New Integer() {18, 0, 0, 0})
+        Me.timebaseNumericUpDown.Name = "timebaseNumericUpDown"
+        Me.timebaseNumericUpDown.Size = New System.Drawing.Size(66, 20)
+        Me.timebaseNumericUpDown.TabIndex = 7
+        Me.timebaseNumericUpDown.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'numSamplesNumericUpDown
+        '
+        Me.numSamplesNumericUpDown.Location = New System.Drawing.Point(329, 98)
+        Me.numSamplesNumericUpDown.Maximum = New Decimal(New Integer() {8192, 0, 0, 0})
+        Me.numSamplesNumericUpDown.Name = "numSamplesNumericUpDown"
+        Me.numSamplesNumericUpDown.Size = New System.Drawing.Size(66, 20)
+        Me.numSamplesNumericUpDown.TabIndex = 8
+        Me.numSamplesNumericUpDown.Value = New Decimal(New Integer() {2000, 0, 0, 0})
+        '
+        'samplingIntervalLabel
+        '
+        Me.samplingIntervalLabel.AutoSize = True
+        Me.samplingIntervalLabel.Location = New System.Drawing.Point(202, 177)
+        Me.samplingIntervalLabel.Name = "samplingIntervalLabel"
+        Me.samplingIntervalLabel.Size = New System.Drawing.Size(90, 13)
+        Me.samplingIntervalLabel.TabIndex = 9
+        Me.samplingIntervalLabel.Text = "Sampling interval:"
+        '
+        'samplingIntervalTextBox
+        '
+        Me.samplingIntervalTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.samplingIntervalTextBox.Location = New System.Drawing.Point(329, 174)
+        Me.samplingIntervalTextBox.Name = "samplingIntervalTextBox"
+        Me.samplingIntervalTextBox.ReadOnly = True
+        Me.samplingIntervalTextBox.Size = New System.Drawing.Size(66, 20)
+        Me.samplingIntervalTextBox.TabIndex = 10
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(202, 210)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(122, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Num. samples collected:"
+        '
+        'numSamplesCollectedTextBox
+        '
+        Me.numSamplesCollectedTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.numSamplesCollectedTextBox.Location = New System.Drawing.Point(329, 207)
+        Me.numSamplesCollectedTextBox.Name = "numSamplesCollectedTextBox"
+        Me.numSamplesCollectedTextBox.ReadOnly = True
+        Me.numSamplesCollectedTextBox.Size = New System.Drawing.Size(66, 20)
+        Me.numSamplesCollectedTextBox.TabIndex = 12
+        '
+        'dataTextBox
+        '
+        Me.dataTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.dataTextBox.Location = New System.Drawing.Point(425, 70)
+        Me.dataTextBox.Multiline = True
+        Me.dataTextBox.Name = "dataTextBox"
+        Me.dataTextBox.ReadOnly = True
+        Me.dataTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.dataTextBox.Size = New System.Drawing.Size(193, 137)
+        Me.dataTextBox.TabIndex = 13
+        '
+        'dataLabel
+        '
+        Me.dataLabel.AutoSize = True
+        Me.dataLabel.Location = New System.Drawing.Point(425, 52)
+        Me.dataLabel.Name = "dataLabel"
+        Me.dataLabel.Size = New System.Drawing.Size(33, 13)
+        Me.dataLabel.TabIndex = 14
+        Me.dataLabel.Text = "Data:"
+        '
+        'PS2000BlockGui
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 261)
+        Me.Controls.Add(Me.dataLabel)
+        Me.Controls.Add(Me.dataTextBox)
+        Me.Controls.Add(Me.numSamplesCollectedTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.samplingIntervalTextBox)
+        Me.Controls.Add(Me.samplingIntervalLabel)
+        Me.Controls.Add(Me.numSamplesNumericUpDown)
+        Me.Controls.Add(Me.timebaseNumericUpDown)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.timebaseLabel)
+        Me.Controls.Add(Me.unitInfoLabel)
+        Me.Controls.Add(Me.getBlockDataButton)
+        Me.Controls.Add(Me.unitInfoTextBox)
+        Me.Controls.Add(Me.closeButton)
+        Me.Controls.Add(Me.openButton)
+        Me.Name = "PS2000BlockGui"
+        Me.Text = "PicoScope 2000 Series Block GUI Capture Example"
+        CType(Me.timebaseNumericUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numSamplesNumericUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents openButton As Button
+    Friend WithEvents closeButton As Button
+    Friend WithEvents unitInfoTextBox As TextBox
+    Friend WithEvents getBlockDataButton As Button
+    Friend WithEvents unitInfoLabel As Label
+    Friend WithEvents timebaseLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents timebaseNumericUpDown As NumericUpDown
+    Friend WithEvents numSamplesNumericUpDown As NumericUpDown
+    Friend WithEvents samplingIntervalLabel As Label
+    Friend WithEvents samplingIntervalTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents numSamplesCollectedTextBox As TextBox
+    Friend WithEvents dataTextBox As TextBox
+    Friend WithEvents dataLabel As Label
+End Class
---
+++ ps2000/PS2000BlockGui/PS2000BlockVBGui.vb
@@ -0,0 +1,323 @@
+﻿'===================================================================================================
+'
+'	Filename:			PS2000BlockVBGui.vb
+'
+'	Description: 
+'	This Windows Form Application demonstrates how to use VB .NET on the PicoScope 2000 Series 
+'   oscilloscopes using the ps2000 driver.
+'
+'   It shows how to connect to a device, display device information, setup the signal generator and 
+'   collect data in block mode.
+'
+'   Copyright (C) 2017 Pico Technology Ltd. See LICENSE file for terms.
+'
+'===================================================================================================
+
+Imports System.IO
+
+Public Class PS2000BlockVBGui
+
+    ' Define variables
+
+    Dim status As Short
+    Dim ps2000Handle As Short
+
+    Dim channelARange As VoltageRange = VoltageRange.PS2000_5V
+    Dim channelBRange As VoltageRange = VoltageRange.PS2000_5V
+
+    ' Arrays for data collection
+    Dim times() As Integer
+    Dim valuesChA() As Short
+    Dim valuesChB() As Short
+
+    ' ******************************************************************************************************************************************************************
+    ' getDeviceInfo - Reads and displays the scopes device information.
+    '
+    ' Parameters: handle - the device handle
+    ' *******************************************************************************************************************************************************************
+    Sub getDeviceInfo(ByVal handle As Short)
+
+        Dim infoText(7) As String
+        Dim infoStr As String
+        Dim i As UInteger
+
+        infoText(0) = "Driver Version:    "
+        infoText(1) = "USB Version:       "
+        infoText(2) = "Hardware Version:  "
+        infoText(3) = "Variant:           "
+        infoText(4) = "Batch / Serial:    "
+        infoText(5) = "Cal Date:          "
+        infoText(6) = "Error code:        "
+        infoText(7) = "Kernel Driver Ver: "
+
+        For i = 0 To infoText.Length - 1
+
+            If i <> 6 Then
+
+                infoStr = "                    "
+                ps2000_get_unit_info(handle, infoStr, CShort(infoStr.Length), i)
+                infoText(i) += infoStr
+
+                unitInfoTextBox.AppendText(infoText(i))
+                unitInfoTextBox.AppendText(vbNewLine)
+
+            End If
+
+        Next i
+
+        Console.WriteLine(vbNewLine)
+
+    End Sub
+
+    Private Sub OpenButton_Click(sender As Object, e As EventArgs) Handles openButton.Click
+
+        ' Device Connection and Setup
+        ' ===========================
+
+        ' Open Device
+        ps2000Handle = ps2000_open_unit()
+
+        unitInfoTextBox.Clear()
+
+        If ps2000Handle = 0 Then
+            MessageBox.Show("Device not opened", "Open Device", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' Display Unit Information
+        getDeviceInfo(ps2000Handle)
+
+        openButton.Enabled = False
+        closeButton.Enabled = True
+        getBlockDataButton.Enabled = True
+
+        ' Turn off ETS
+        Call ps2000_set_ets(ps2000Handle, 0, 0, 0) ' Turn ETS off
+
+        ' Set Channels
+        ' ------------
+
+        status = ps2000_set_channel(ps2000Handle, Channel.PS2000_CHANNEL_A, CShort(1), CShort(1), channelARange) ' Channel A: enabled DC coupling 5V range
+        status = ps2000_set_channel(ps2000Handle, Channel.PS2000_CHANNEL_B, CShort(0), CShort(1), channelBRange) ' Channel B: not enabled DC coupling 5V range
+
+    End Sub
+
+    Private Sub ErrorHandler(ByVal handle As Short, ByVal functionName As String)
+
+        Dim infoStr As String
+        Dim message As String
+        Dim caption As String
+        Dim buttons As MessageBoxButtons = MessageBoxButtons.OK
+        Dim icon As MessageBoxIcon = MessageBoxIcon.Error
+
+        infoStr = "                    "
+        ps2000_get_unit_info(handle, infoStr, CShort(infoStr.Length), Info.PS2000_ERROR_CODE)
+
+        message = String.Concat("Error: ", functionName, " was not called correctly.")
+        caption = "Error Calling Function"
+
+        MessageBox.Show(message, caption, buttons, icon)
+
+    End Sub
+
+    Private Function TimeUnitsToString(ByVal timeUnits As TimeUnits) As String
+
+        Select Case timeUnits
+
+            Case PS2000Imports.TimeUnits.PS2000_FS
+                Return "fs"
+
+            Case PS2000Imports.TimeUnits.PS2000_PS
+                Return "ps"
+
+            Case PS2000Imports.TimeUnits.PS2000_NS
+                Return "ns"
+
+            Case PS2000Imports.TimeUnits.PS2000_US
+                Return "us"
+
+            Case PS2000Imports.TimeUnits.PS2000_MS
+                Return "ms"
+
+            Case PS2000Imports.TimeUnits.PS2000_S
+                Return "s"
+
+            Case Else
+
+                Return ""
+
+        End Select
+
+    End Function
+
+    Private Sub GetBlockDataButton_Click(sender As Object, e As EventArgs) Handles getBlockDataButton.Click
+
+        samplingIntervalTextBox.Clear()
+        numSamplesCollectedTextBox.Clear()
+        dataTextBox.Clear()
+
+        ' Verify Timebase for Sampling Interval
+        ' -------------------------------------
+
+        'Find the maximum number of samples the time interval (in TimeUnits)
+        'the most suitable time units and the maximum oversample at the current timebase
+
+        Dim timebaseStatus As Short = 0
+        Dim timebase As Short = CShort(timebaseNumericUpDown.Value)
+        Dim numSamples As Integer = CInt(numSamplesNumericUpDown.Value)
+        Dim timeInterval As Integer
+        Dim timeUnits As TimeUnits
+        Dim oversample As Short = 1
+        Dim maxSamples As Integer = 0
+
+        Do Until timebaseStatus = 1
+
+            timebaseStatus = ps2000_get_timebase(ps2000Handle, timebase, numSamples, timeInterval, timeUnits, oversample, maxSamples)
+
+            If timebaseStatus = 0 Then
+
+                timebase = timebase + 1
+
+            End If
+
+        Loop
+
+        samplingIntervalTextBox.AppendText(String.Concat(timeInterval, " ", TimeUnitsToString(timeUnits)))
+
+        ' Setup trigger
+        ' -------------
+
+        ' Setup a trigger such that the device will trigger on a rising edge through 500 mV on Channel A, auto trigger set to 5 seconds
+
+        Dim threshold As Short = 0
+
+        threshold = mvToAdc(500, channelARange, PS2000Imports.PS2000_MAX_VALUE)
+
+        status = ps2000_set_trigger2(ps2000Handle, Channel.PS2000_CHANNEL_A, threshold, CShort(0), CSng(-50.0), 5000)
+
+        ' Signal Generator
+        ' ================
+
+        ' Display message prompting user to connect signal generator to channel A
+
+        Dim message As String = "Connect the AWG output to Channel A and click OK to continue."
+        Dim caption As String = "Signal Generator"
+        Dim button As MessageBoxButtons = MessageBoxButtons.OK
+        Dim icon As MessageBoxIcon = MessageBoxIcon.None
+
+        MessageBox.Show(message, caption, button, icon)
+
+        ' Use in-built function generator in order to provide a test signal (1 kHz, 2 Vpp sine wave)
+
+        Dim offsetVoltage As Integer = 0
+        Dim peakToPeak As UInteger = 2000000 ' +/- 1 Volt
+        Dim waveType As WaveType = WaveType.PS2000_SINE
+        Dim startFrequency As Single = 1000.0 ' Hertz
+        Dim stopFrequency As Single = 1000.0 ' Hertz
+        Dim increment As Single = 0.0
+        Dim dwellTime As Single = 0.0
+        Dim sweepType As SweepType = SweepType.PS2000_UP
+        Dim numSweeps As UInteger = 0
+
+        status = ps2000_set_sig_gen_built_in(ps2000Handle, offsetVoltage, peakToPeak, waveType, startFrequency, stopFrequency, increment, dwellTime, sweepType, numSweeps)
+
+        If status = 0 Then
+
+            Call ErrorHandler(ps2000Handle, "ps2000_set_sig_gen_built_in")
+
+        End If
+
+        ' Data Capture
+        ' ============
+
+        ' Capture block of data from the device using a trigger.
+
+        Dim timeIndisposedMs As Integer = 0
+        Dim isReady As Short = 0
+
+        ' Start device collecting data then wait for completion
+        status = ps2000_run_block(ps2000Handle, numSamples, timebase, oversample, timeIndisposedMs)
+
+        If status = 0 Then
+
+            Call ErrorHandler(ps2000Handle, "ps2000_run_block")
+
+        End If
+
+        While isReady = 0
+
+            isReady = ps2000_ready(ps2000Handle)
+            Sleep(5)
+
+        End While
+
+        If isReady = 1 Then
+
+            ' Set up buffers to collect time and channel data
+            ReDim times(numSamples - 1)
+            ReDim valuesChA(numSamples - 1)
+            ReDim valuesChB(numSamples - 1)
+
+            Dim overflow As Short
+            Dim numSamplesCollected As Integer = 0
+
+            ' Retrieve the number samples collected, time and channel data
+            numSamplesCollected = ps2000_get_times_and_values(ps2000Handle, times(0), valuesChA(0), valuesChB(0), Nothing, Nothing, overflow, timeUnits, numSamples)
+
+            If numSamplesCollected = 0 Then
+
+                Call ErrorHandler(ps2000Handle, "ps2000_get_times_and_values")
+
+            Else
+
+                numSamplesCollectedTextBox.AppendText(numSamplesCollected)
+
+            End If
+
+            ' Process the Data
+            ' ----------------
+
+            ' In this example, the data is converted to millivolts and output to file
+
+            Dim valuesChAMv() As Single
+            ReDim valuesChAMv(numSamplesCollected - 1)
+
+            dataTextBox.AppendText(String.Concat("Time (", TimeUnitsToString(timeUnits), ")", vbTab, vbTab, "Ch. A"))
+            dataTextBox.AppendText(vbNewLine)
+
+            For index = 0 To numSamplesCollected - 1
+
+                valuesChAMv(index) = PicoFunctions.adcToMv(valuesChA(index), channelARange, PS2000_MAX_VALUE) ' Use the voltage range specified in the call to ps2000_set_channel
+
+                dataTextBox.AppendText(String.Concat(times(index).ToString, vbTab, valuesChAMv(index).ToString))
+                dataTextBox.AppendText(vbNewLine)
+
+            Next
+
+
+
+        Else
+
+            MessageBox.Show("Data collection cancelled.", "Data Collection", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        End If
+
+        ' Reset isReady flag if we need to collect another block of data
+        isReady = 0
+
+        ' Stop the device
+        ps2000_stop(ps2000Handle)
+
+    End Sub
+
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
+
+        ' Close the connection to the device
+        Call ps2000_close_unit(ps2000Handle)
+
+        openButton.Enabled = True
+        getBlockDataButton.Enabled = False
+
+    End Sub
+
+End Class
